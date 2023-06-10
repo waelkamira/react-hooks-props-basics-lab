efbaa97acaadf72feecc65676ec7e40efabc8724
wael kamira <ramondm4@gmail.com>
2023-06-10 21:18:00 +0300

    Assignment Completed

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} name={user.name} city={user.city}/>
+      <About github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+function Links(props) {
+  console.log(props.github);
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a>
+    </div>
+  );
+}
+export default Links;
